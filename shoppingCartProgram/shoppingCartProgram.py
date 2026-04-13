da7b25956f22cc7a0ab81a3f81291108ec884a66
Sudipta <sudiptabhatta34@gmail.com>
2026-04-13 23:04:02 +0530

    complete the project

--- shoppingCartProgram/shoppingCartProgram.py
+++ shoppingCartProgram/shoppingCartProgram.py
@@ -2,3 +2,18 @@
 foods = []
 prices = []
 total = 0
+
+while True:
+    food = input("Enter your food item and press q to quit: ")
+    if food.lower() =="q" :
+        break
+    price = float(input("Enter the price: "))
+    foods.append(food)
+    prices.append(price)
+
+print("--Your Cart--")
+for x in range(len(foods)) :
+    print(f"{foods[x]} ${prices[x]}")
+    total +=prices[x]
+
+print(f"Total price: ${float(total)}")
